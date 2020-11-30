754c9b50b54374b138c6337e3dd30220bcbd3e4f
Gopal Penny <gopalpenny@gmail.com>
2020-11-29 21:25:25 -0500

    ganges sreamflow data now goes through june

--- data-raw/ganges_streamflow.R
+++ data-raw/ganges_streamflow.R
@@ -10,25 +10,30 @@ Q_treaty_hardinge_avg <- Q_obs %>%
   dplyr::filter(date >= "1998-01-01") %>%
   dplyr::group_by(yearless_date) %>%
   dplyr::summarize(Q_cumec = mean(Q_cumec)) %>%
-  dplyr::filter(yearless_date <= "0000-05-31", yearless_date != as.Date("0000-02-29")) %>%
+  dplyr::filter(yearless_date <= "0000-06-30", yearless_date != as.Date("0000-02-29")) %>%
   dplyr::mutate(date = as.Date(strftime(yearless_date,"1998-%m-%d")),
                 yday = as.numeric(strftime(date,"%j")),
                 year = 1,
-                group = "Treaty avg") %>%
-  select(-yearless_date)
-
+                group = "Treaty avg")
 # Prepare the data
 Q_ts <- Q_synth %>% dplyr::pull(predOLSNO_Q050) # select the median quantile, Q050
 Q_dates <- Q_synth %>% dplyr::pull(date)
 Q_dates <- Q_synth %>% dplyr::pull(date)
 Q_prepped <- sim_salin_prep(Q_ts, pred_dates=Q_dates, Q_obs_df=Q_obs) %>%
-  dplyr::filter(year == 2006, date <= "2006-05-31") %>%
-  dplyr::mutate(group = "2006 synthetic, no treaty")
+  dplyr::filter(year == 2006, date <= "2006-06-30") %>%
+  left_join(Q_treaty_hardinge_avg %>% select(yearless_date, Q_treaty_avg = Q_cumec), by = "yearless_date")#
+
+# shift 2006 data to follow treaty average in June
+shift_2006 <- Q_treaty_hardinge_avg$Q_cumec[Q_treaty_hardinge_avg$yearless_date == "0000-05-31"] -
+  Q_prepped$Q_cumec[Q_prepped$date == "2006-05-31"]
+Q_prepped <- Q_prepped %>%
+  dplyr::mutate(group = "2006 synthetic, no treaty",
+                Q_cumec = if_else(date > as.Date("2006-05-31"), Q_treaty_avg - shift_2006, pred_smooth))
 
 ganges_streamflow <- Q_treaty_hardinge_avg %>%
-  bind_rows(Q_prepped %>% dplyr::select(Q_cumec = pred_smooth,year,yday,group, date)) %>%
+  bind_rows(Q_prepped %>% dplyr::select(Q_cumec,year,yday,group, date)) %>%
   dplyr::select(year, date, yday, Q_cumec, group)
 
-# ggplot(streamflow_df) + geom_line(aes(yday,Q_cumec,color = group))
+# ggplot(ganges_streamflow) + geom_line(aes(yday,Q_cumec,color = group))
 
 usethis::use_data(ganges_streamflow, overwrite = TRUE)
