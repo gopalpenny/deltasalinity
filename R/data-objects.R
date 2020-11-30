377e9f2e49ba38956ae0cfe23d5483e9e8ff5fa2
Gopal Penny <gopalpenny@gmail.com>
2020-11-29 22:33:02 -0500

    added data documentation

---
+++ R/data-objects.R
@@ -0,0 +1,13 @@
+# data-objects
+
+#' Ganges calibrated parameters
+#'
+#' This data contains calibrated parameters for modeling Ganges salinity at Khulna
+"ganges_params"
+
+
+#' Ganges streamflow data
+#'
+#' This data contains daily average streamflow data over the time period of the treaty (after 1998)
+#' as well as synthetic data from 2006 without a treaty.
+"ganges_streamflow"
