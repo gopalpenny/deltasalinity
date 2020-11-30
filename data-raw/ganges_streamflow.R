918290a559c9925f314579163faef61c1bca73be
Gopal Penny <gopalpenny@gmail.com>
2020-11-29 19:20:00 -0500

    removed Feb 29 leap day from treaty data

--- data-raw/ganges_streamflow.R
+++ data-raw/ganges_streamflow.R
@@ -5,19 +5,21 @@
 Q_obs <- read_csv("./ignore/ganges_streamflow/Q_observed_filtered.csv")
 Q_synth <- read_csv("./ignore/ganges_streamflow/synthetic_streamflow_quantiles_0_100.csv")
 
-Q_treaty_hardinge_avg <- Q_obs %>% dplyr::mutate(yearless_date = as.Date(strftime(date,"0000-%m-%d"))) %>%
+Q_treaty_hardinge_avg <- Q_obs %>%
+  dplyr::mutate(yearless_date = as.Date(strftime(date,"0000-%m-%d"))) %>%
   dplyr::filter(date >= "1998-01-01") %>%
   dplyr::group_by(yearless_date) %>%
   dplyr::summarize(Q_cumec = mean(Q_cumec)) %>%
-  dplyr::mutate(yday = as.numeric(strftime(yearless_date,"%j")),
+  dplyr::filter(yearless_date <= "0000-05-31", yearless_date != as.Date("0000-02-29")) %>%
+  dplyr::mutate(date = as.Date(strftime(yearless_date,"1998-%m-%d")),
+                yday = as.numeric(strftime(date,"%j")),
                 year = 1,
                 group = "Treaty avg") %>%
-  dplyr::filter(yearless_date <= "0000-05-31") %>%
-  dplyr::mutate(date = as.Date(strftime(yearless_date,"1998-%m-%d"))) %>%
   select(-yearless_date)
 
 # Prepare the data
 Q_ts <- Q_synth %>% dplyr::pull(predOLSNO_Q050) # select the median quantile, Q050
+Q_dates <- Q_synth %>% dplyr::pull(date)
 Q_dates <- Q_synth %>% dplyr::pull(date)
 Q_prepped <- sim_salin_prep(Q_ts, pred_dates=Q_dates, Q_obs_df=Q_obs) %>%
   dplyr::filter(year == 2006, date <= "2006-05-31") %>%
