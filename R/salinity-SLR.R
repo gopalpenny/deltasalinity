c910210b45306bb3c7cefa441d412b7fce48bfe7
Gopal Penny <gopalpenny@gmail.com>
2020-11-30 22:12:22 -0500

    added get_SLR_channel_ratios

---
+++ R/salinity-SLR.R
@@ -0,0 +1,17 @@
+# salinity-SLR.R
+
+#' Get channel volume ratios
+#'
+#' Get channel volume ratios under sea level rise
+#' @param SLR_m vector of sea level rise in meters
+#' @export
+#' @examples
+#' channel_ratios <- get_SLR_channel_ratios(SLR_m = seq(0,1,by = 0.05))
+#' ggplot(channel_ratios) +
+#'   geom_line(aes(SLR_m, R_E, color = "R_E")) +
+#'   geom_line(aes(SLR_m, R_V, color = "R_V")) + ylab("Ratio")
+get_SLR_channel_ratios <- function(SLR_m) {
+  R_V = 0.1647 * SLR_m + 1
+  R_E = 0.0694 * SLR_m + 1
+  return(data.frame(SLR_m = SLR_m, R_V = R_V, R_E = R_E))
+}
