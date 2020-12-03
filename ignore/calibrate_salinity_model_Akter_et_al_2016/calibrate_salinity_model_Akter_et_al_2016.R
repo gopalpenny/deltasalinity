7b8b33ff28914614865ffa85607fd9411d00cd09
Gopal Penny <gopalpenny@gmail.com>
2020-12-02 20:35:28 -0500

    updated akter calibration for a and b

--- ignore/calibrate_salinity_model_Akter_et_al_2016/calibrate_salinity_model_Akter_et_al_2016.R
+++ ignore/calibrate_salinity_model_Akter_et_al_2016/calibrate_salinity_model_Akter_et_al_2016.R
@@ -1,3 +1,6 @@
+library(readr)
+library(dplyr)
+
 Q_obs_2011 <- read_csv("./ignore/ganges_streamflow/Q_observed_filtered.csv") %>%
   filter(date >= "2011-01-01", date<="2011-06-30")
 
@@ -9,11 +12,17 @@ treat_salinity_54cm <- Q_obs_2011 %>%
 
 ### Update calibration parameters a and b
 v_ab_channel_slr54 <- update_ab_SLR(ganges_params$param, SLR_m = 0.54, "channel")
+### Update calibration parameters a and b
+v_ab_gw_slr54 <- update_ab_SLR(ganges_params$param, SLR_m = 0.54, "gw")
 
 ### Calibrate parameter d
 # sea level rise of 54 cm
-v_slr54 <- treat_salinity_54cm %>% rename(S_ppm = S_ppm_SLR_calibrate) %>%
+v_slr54_channel <- treat_salinity_54cm %>% rename(S_ppm = S_ppm_SLR_calibrate) %>%
   calibrate_salinity_model(v_ab_channel_slr54, control = list(trace = FALSE))
+v_slr54_gw <- treat_salinity_54cm %>% rename(S_ppm = S_ppm_SLR_calibrate) %>%
+  calibrate_salinity_model(v_ab_gw_slr54, control = list(trace = FALSE))
 
-# paste(v_slr54,collapse=",")
-# v_slr54 <- c(-12.645827591134,-4.25586229021828,-6.64061943030275,10.4631033404715)
+paste(v_slr54_channel,collapse=",")
+# v_slr54_channel <- c(-12.645827591134,-4.25586229021828,-6.64061943030275,10.4631033404715)
+paste(v_slr54_gw,collapse=",")
+# v_slr54_gw <-      c(-12.607436700222,-4.25426223963173,-6.64714042324189,10.4631033404715)
